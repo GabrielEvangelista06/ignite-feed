6510330ef5794d6337cddf156fd3ac1a8b8e2291
Gabriel Evangelista <gabrielevangelista094@gmail.com>
2024-09-12 15:02:49 -0300

    creating sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import "./global.css";
 import { Post } from "./Post";
-import "./styles.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
-      <Post
-        author="Gabriel Evangelista"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla eius perferendis nesciunt sunt! Explicabo sunt soluta ullam aut dicta perferendis repudiandae exercitationem. Facilis sequi nobis quam praesentium illum deleniti non."
-      />
-      <Post
-        author="Claudete Morais"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla eius perferendis nesciunt sunt! Explicabo sunt soluta ullam aut dicta perferendis repudiandae exercitationem. Facilis sequi nobis quam praesentium illum deleniti non."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel Evangelista"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla eius perferendis nesciunt sunt! Explicabo sunt soluta ullam aut dicta perferendis repudiandae exercitationem. Facilis sequi nobis quam praesentium illum deleniti non."
+          />
+          <Post
+            author="Claudete Morais"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla eius perferendis nesciunt sunt! Explicabo sunt soluta ullam aut dicta perferendis repudiandae exercitationem. Facilis sequi nobis quam praesentium illum deleniti non."
+          />
+        </main>
+      </div>
     </>
   );
 }
